94f57c866482aca1796a683258c97fbc6ae23a1a
aka256 <redmark.2048@gmail.com>
2021-07-02 23:54:13 +0900

    setup.pyにNBTを追加

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   description='Package of creating othello AI for Minecraft datapack with reinforcement learning',
   long_description=readme,
   author='aka',
-  install_requires=['matplotlib'],
+  install_requires=['matplotlib', 'NBT'],
   url='https://github.com/aka256/othello-rl',
   license=license,
   packages=find_packages(exclude=('tests', 'docs'))
